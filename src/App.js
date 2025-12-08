2b592eee7b2bb8c221fe3a07fde4b49941a0ffa7
Daniel Aleman robaina <nadinnear@gmail.com>
2025-12-08 23:22:01 +0000

    Update cloud settings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { Clouds, Cloud, OrbitControls } from "@react-three/drei";
-
 function CloudScene() {
   return (
-    <Canvas
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "block",
-      }}
-      camera={{ position: [0, 2, 12], fov: 55 }}
-    >
+    <Canvas camera={{ position: [0, 1.5, 10], fov: 40 }}>
       <color attach="background" args={["#050816"]} />
       <fog attach="fog" args={["#050816", 5, 20]} />
 
@@ -20,40 +9,71 @@ function CloudScene() {
       <directionalLight position={[5, 10, 5]} intensity={1} />
 
       {/* Clouds */}
-      <Clouds>
+      <Clouds material="phong">
+
+        {/* Left big cloud */}
+        <Cloud
+          segments={80}
+          bounds={[6, 3, 2]}
+          volume={8}
+          opacity={0.9}
+          speed={0.15}
+          color="#ffffff"
+          fade={60}
+          position={[-4, -1, 0]}
+        />
+
+        {/* Left mid cloud */}
         <Cloud
           segments={60}
-          bounds={[4, 2, 2]}
-          volume={7}
-          opacity={0.8}
-          speed={0.2}
-          color="#ffffff"
-          fade={40}
-          position={[0, 1.5, 0]}
+          bounds={[4, 2.5, 2]}
+          volume={6}
+          opacity={0.85}
+          speed={0.18}
+          color="#b4d5ff"
+          fade={45}
+          position={[-2.2, -0.8, -0.5]}
         />
+
+        {/* Right big cloud */}
+        <Cloud
+          segments={80}
+          bounds={[6, 3, 2]}
+          volume={8}
+          opacity={0.9}
+          speed={0.15}
+          color="#ffffff"
+          fade={60}
+          position={[4, -1, 0]}
+        />
+
+        {/* Right mid cloud */}
+        <Cloud
+          segments={60}
+          bounds={[4, 2.5, 2]}
+          volume={6}
+          opacity={0.85}
+          speed={0.18}
+          color="#b4d5ff"
+          fade={45}
+          position={[2.2, -0.8, -0.5]}
+        />
+
+        {/* Soft center fog to blend the gap */}
         <Cloud
           segments={40}
-          bounds={[4, 2, 2]}
-          volume={5}
-          opacity={0.6}
-          speed={0.25}
-          color="#9fc9ff"
-          fade={35}
-          position={[-3, 1, -2]}
+          bounds={[3, 1, 2]}
+          volume={2}
+          opacity={0.4}
+          speed={0.1}
+          color="#ffffff"
+          fade={30}
+          position={[0, -0.3, -1]}
         />
+
       </Clouds>
 
       <OrbitControls enableZoom={false} enablePan={false} />
     </Canvas>
-  );
-}
-
-export default function App() {
-  return (
-    <div className="App">
-      <div className="canvas-wrapper">
-        <CloudScene />
-      </div>
-    </div>
   );
 }
